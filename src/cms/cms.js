445021a629cc27cf3406532d22f3b758dbdf518b
marco-esposito <m.espo@tiscali.it>
2018-06-09 15:31:36 +0200

    test su preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,9 +3,10 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
+import BlackdeePagePreview from './preview-templates/BlackdeePagePreview'
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
+// CMS.registerPreviewTemplate('products', ProductPagePreview)
+// CMS.registerPreviewTemplate('blackdee', BlackdeePagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
