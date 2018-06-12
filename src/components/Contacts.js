ce5910374e6826cc69a86335b695e258aa05716e
marco-esposito <m.espo@tiscali.it>
2018-06-12 15:22:12 +0200

    test form

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,54 +2,26 @@ import React, { Fragment } from 'react'
 
 const renderForm = () => (
   <Fragment>
-    <form
-      name="contact"
-      method="post"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-    >
-      <div className="columns">
-          <div className="column is-5">
-            <div className="field">
-              <label className="label">Name and surname</label>
-              <div className="control">
-                <input className="input" name="name" autoComplete="name" type="text" placeholder="Your text here" />
-              </div>
-            </div>
-            <div className="field">
-              <label className="label">Company</label>
-              <div className="control">
-                <input className="input" type="text" name="company" autoComplete="company" placeholder="Your text here" />
-              </div>
-            </div>
-            <div className="field">
-              <label className="label">Email</label>
-              <div className="control has-icons-left has-icons-right">
-                <input className="input is-danger" type="email" name="email" autoComplete="off" placeholder="Your text here" />
-                <span className="icon is-small is-left">
-                  <i className="fas fa-envelope"></i>
-                </span>
-                <span className="icon is-small is-right">
-                  <i className="fas fa-exclamation-triangle"></i>
-                </span>
-              </div>
-              <p className="help is-danger">This email is invalid</p>
-            </div>
-          </div>
-          <div className="column is-5">
-            <div className="field">
-              <label className="label">Message</label>
-              <div className="control">
-                <textarea className="textarea" placeholder="Textarea" rows="10"></textarea>
-              </div>
-            </div>
-          </div>
-      </div>
-      <div className="field is-grouped">
-        <div className="control">
-          <button className="button is-link" type="submit">Submit</button>
-        </div>
-      </div>
+    <form name="contact" method="POST" netlify>
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Your Role: <select name="role[]" multiple>
+          <option value="leader">Leader</option>
+          <option value="follower">Follower</option>
+        </select></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
     </form>
   </Fragment>
   )
