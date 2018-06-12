c371679d459ec76b375bf4e9de80549168deed51
marco-esposito <m.espo@tiscali.it>
2018-06-12 14:32:14 +0200

    sample contact form linked to netlify

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,47 +2,50 @@ import React, { Fragment } from 'react'
 
 const renderForm = () => (
   <Fragment>
-    <div className="columns">
-      <div className="column is-5">
-        <div className="field">
-          <label className="label">Name</label>
-          <div className="control">
-            <input className="input" type="text" placeholder="Name and surname" />
+    <form
+      name="contact"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <div className="columns">
+          <div className="column is-5">
+            <div className="field">
+              <label className="label">Name and surname</label>
+              <div className="control">
+                <input className="input" name="name" autoComplete="name" type="text" placeholder="Your text here" />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Company</label>
+              <div className="control">
+                <input className="input" type="text" name="company" autoComplete="company" placeholder="Your text here" />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control has-icons-left has-icons-right">
+                <input className="input is-danger" type="email" name="email" autoComplete="off" placeholder="Your text here" />
+                <span className="icon is-small is-left">
+                  <i className="fas fa-envelope"></i>
+                </span>
+                <span className="icon is-small is-right">
+                  <i className="fas fa-exclamation-triangle"></i>
+                </span>
+              </div>
+              <p className="help is-danger">This email is invalid</p>
+            </div>
           </div>
-        </div>
-
-        <div className="field">
-          <label className="label">Company</label>
-          <div className="control">
-            <input className="input" type="text" placeholder="Company" />
+          <div className="column is-5">
+            <div className="field">
+              <label className="label">Message</label>
+              <div className="control">
+                <textarea className="textarea" placeholder="Textarea" rows="10"></textarea>
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div className="field">
-          <label className="label">Email</label>
-          <div className="control has-icons-left has-icons-right">
-            <input className="input is-danger" type="email" placeholder="Email" />
-            <span className="icon is-small is-left">
-              <i className="fas fa-envelope"></i>
-            </span>
-            <span className="icon is-small is-right">
-              <i className="fas fa-exclamation-triangle"></i>
-            </span>
-          </div>
-          <p className="help is-danger">This email is invalid</p>
-        </div>
       </div>
-
-      <div className="column is-5">
-        <div className="field">
-          <label className="label">Message</label>
-          <div className="control">
-            <textarea className="textarea" placeholder="Textarea" rows="10"></textarea>
-          </div>
-        </div>
-      </div>
-
-    </div>
+    </form>
 
     <div className="field is-grouped">
       <div className="control">
@@ -65,7 +68,7 @@ const Contacts = () => (
               <h1>Contacts</h1>
               <p>Cras quis nulla commodo, aliquam lectus sed, blandit augue.</p>
             </div>
-            <div className="section-content">
+            <div className="contact-form">
               {renderForm()}
             </div>
           </div>
