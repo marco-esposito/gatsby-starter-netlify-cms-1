1ddbb92bc362cfd0255dafa04278b36be092d6ee
marco-esposito <m.espo@tiscali.it>
2018-06-12 19:12:24 +0200

    validation fully implemented

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -34,7 +34,8 @@ class Contacts extends Component {
     });
   }
 
-  handleIncompleteField = evt => {
+  // Check empty fields when user blur
+  handleBlurField = evt => {
     if (!evt.target.value.length) {
       this.setState({
         ...this.state,
@@ -63,11 +64,50 @@ class Contacts extends Component {
   }
 
   handleValidation = evt => {
+    // Activate "is-danger" on empty fields
+    // useful when user clicks on submit without entering any field
+    const formValues = this.state.formValues;
+    const isDangerClassObject = Object.keys(formValues).reduce((acc, currValue) => {
+      if (!formValues[currValue].length) acc[currValue] = 'is-danger';
+      return acc;
+    }, {})
+    const isHiddenClassObject = _.mapValues(isDangerClassObject, () => '');
+    this.setState({
+      ...this.state,
+      isDangerClass: {
+        ...this.state.isDangerClass,
+        ...isDangerClassObject,
+      },
+      isHiddenClass: {
+        ...this.state.isHiddenClass,
+        ...isHiddenClassObject,
+      }
+    });
+
+    // Prevent to submit if compulsory fields are empty
     const emptyValues = _.filter(this.state.formValues, value => !value.length);
     if (emptyValues.length) {
       evt.preventDefault();
     };
   }
+
+  renderInputField = (label, name, type) => (
+    <div className="field">
+      <label className="label">{label}</label>
+      <div className="control">
+        <input
+          className={`input ${this.state.isDangerClass[name]}`}
+          name={name}
+          autoComplete={name}
+          type={type}
+          placeholder="Your text here"
+          onChange={this.handleChangeField}
+          onBlur={this.handleBlurField}
+        />
+      </div>
+      <p className={`help ${this.state.isDangerClass[name]} ${this.state.isHiddenClass[name]}`}>This is a compulsory field</p>
+    </div>
+  )
 
   renderForm = () => (
   <Fragment>
@@ -80,51 +120,9 @@ class Contacts extends Component {
       <input type="hidden" name="form-name" value="contact" />
       <div className="columns">
           <div className="column is-5">
-            <div className="field">
-              <label className="label">Name and surname</label>
-              <div className="control">
-                <input
-                  className={`input ${this.state.isDangerClass.name}`}
-                  name="name"
-                  autoComplete="name"
-                  type="text"
-                  placeholder="Your text here"
-                  onChange={this.handleChangeField}
-                  onBlur={this.handleIncompleteField}
-                />
-              </div>
-              <p className={`help ${this.state.isDangerClass.name} ${this.state.isHiddenClass.name}`}>This is a compulsory field</p>
-            </div>
-            <div className="field">
-              <label className="label">Company</label>
-              <div className="control">
-                <input
-                  className={`input ${this.state.isDangerClass.company}`}
-                  type="text"
-                  name="company"
-                  autoComplete="company"
-                  placeholder="Your text here"
-                  onChange={this.handleChangeField}
-                  onBlur={this.handleIncompleteField}
-                />
-              </div>
-              <p className={`help ${this.state.isDangerClass.company} ${this.state.isHiddenClass.company}`}>This is a compulsory field</p>
-            </div>
-
-            <div className="field">
-              <label className="label">Email</label>
-              <div className="control">
-                <input
-                  className={`input ${this.state.isDangerClass.email}`}
-                  name="email"
-                  type="email"
-                  placeholder="Your text here"
-                  onChange={this.handleChangeField}
-                  onBlur={this.handleIncompleteField}
-                />
-              </div>
-              <p className={`help ${this.state.isDangerClass.email} ${this.state.isHiddenClass.email}`}>This is a compulsory field</p>
-            </div>
+            {this.renderInputField('Name and surname', 'name', 'text')}
+            {this.renderInputField('Company', 'company', 'text')}
+            {this.renderInputField('Email', 'email', 'email')}
           </div>
           <div className="column is-5">
             <div className="field">
@@ -136,7 +134,7 @@ class Contacts extends Component {
                   placeholder="Textarea"
                   rows="10"
                   onChange={this.handleChangeField}
-                  onBlur={this.handleIncompleteField}
+                  onBlur={this.handleBlurField}
                 >
                 </textarea>
               </div>
