43b418bbf7f80f901fd39c231adab305805a7bd8
marco-esposito <m.espo@tiscali.it>
2018-06-13 13:44:18 +0200

    TextArea added

---
+++ src/components/Contacts/TextArea.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TextArea = ( { className, name, onChangeField, onBlurField } ) => (
+  <textarea
+    className={className}
+    name={name}
+    placeholder="Your text here"
+    rows="10"
+    onChange={onChangeField}
+    onBlur={onBlurField}
+  />
+)
+//
+export default TextArea;
